fd1d361efb0f0bfc1470a76f218347ccb02ea610
Piotr P. <piotrek@jakbyco.com>
2014-10-29 23:02:20 +0100

    add swipebox

---
+++ js/shops.js
@@ -0,0 +1,77 @@
+      $( document ).ready(function() {
+        $( '.swipebox' ).swipebox();
+
+        $( '#nr1' ).click( function( e ) {
+          e.preventDefault();
+        $.swipebox( [
+          { href:'img/shops/1.jpg', title:'My Caption' }, 
+          { href:'img/shops/2.jpg', title:'My Caption' }
+          ] );
+        } );
+
+        $( '#nr2' ).click( function( e ) {
+          e.preventDefault();
+        $.swipebox( [
+          { href:'img/shops/2.jpg', title:'My Caption' }, 
+          { href:'img/shops/2.jpg', title:'My Caption' }
+          ] );
+        } );
+
+        $( '#nr3' ).click( function( e ) {
+          e.preventDefault();
+        $.swipebox( [
+          { href:'img/shops/2.jpg', title:'My Caption' }, 
+          { href:'img/shops/2.jpg', title:'My Caption' }
+          ] );
+        } );
+
+        $( '#nr4' ).click( function( e ) {
+          e.preventDefault();
+        $.swipebox( [
+          { href:'img/shops/1.jpg', title:'My Caption' }, 
+          { href:'img/shops/2.jpg', title:'My Caption' }
+          ] );
+        } );
+
+        $( '#nr5' ).click( function( e ) {
+          e.preventDefault();
+        $.swipebox( [
+          { href:'img/shops/2.jpg', title:'My Caption' }, 
+          { href:'img/shops/2.jpg', title:'My Caption' }
+          ] );
+        } );
+
+        $( '#nr6' ).click( function( e ) {
+          e.preventDefault();
+        $.swipebox( [
+          { href:'img/shops/2.jpg', title:'My Caption' }, 
+          { href:'img/shops/2.jpg', title:'My Caption' }
+          ] );
+        } );
+
+        $( '#nr7' ).click( function( e ) {
+          e.preventDefault();
+        $.swipebox( [
+          { href:'img/shops/1.jpg', title:'My Caption' }, 
+          { href:'img/shops/2.jpg', title:'My Caption' }
+          ] );
+        } );
+
+        $( '#nr8' ).click( function( e ) {
+          e.preventDefault();
+        $.swipebox( [
+          { href:'img/shops/2.jpg', title:'My Caption' }, 
+          { href:'img/shops/2.jpg', title:'My Caption' }
+          ] );
+        } );
+
+        $( '#nr9' ).click( function( e ) {
+          e.preventDefault();
+        $.swipebox( [
+          { href:'img/shops/2.jpg', title:'My Caption' }, 
+          { href:'img/shops/2.jpg', title:'My Caption' }
+          ] );
+        } );
+
+
+      });
